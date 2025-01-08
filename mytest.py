95dd2bbd8ece98dd70069c7732ccce79dd9024bd
Vincent Beraudier <vincent.beraudier@fr.ibm.com>
2025-01-08 18:45:55 +0100

    Create mytest.py

---
+++ mytest.py
@@ -0,0 +1,6 @@
+items = {"Coffee": 2.2, "Tea": 1.5, "Chocolate": 2.5}
+for item in items.keys()
+     income = 0
+      qty = input(f"How many {item}s have you sold? ")
+     income = income + qty * items[item]
+print(f"\nThe income today was £{income:0.2f}")
